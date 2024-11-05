6b48c09223ad613cdf4fc77aab2272008014ff36
dev-karunendu-mishra <166835818+dev-karunendu-mishra@users.noreply.github.com>
2024-11-06 02:50:48 +0530

    checking public path

--- resources/views/dashboard/auth/guest.blade.php
+++ resources/views/dashboard/auth/guest.blade.php
@@ -185,6 +185,13 @@
 
         <!-- Page JS -->
         <script src="{{ asset('/assets/dashboard/js/pages-auth.js') }}"></script>
+        <!-- Helpers -->
+    <script src="{{ asset('/assets/dashboard/vendor/js/helpers.js') }}"></script>
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
+    <script src="{{ asset('/assets/dashboard/vendor/js/template-customizer.js') }}"></script>
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="{{ asset('/assets/dashboard/js/config.js') }}"></script>
     </body>
 </html>
 
