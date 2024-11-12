e7e0f2591b25ab87cabafb005a369dda0e1f6999
dev-karunendu-mishra <166835818+dev-karunendu-mishra@users.noreply.github.com>
2024-11-13 03:32:26 +0530

    locale check

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,7 @@
 @hasrole('admin')
 I'm admin
 @else
-I'm not admin<img src="{{asset('assets/dashboard/img/pages/header.png')}}"/>
+{{ __('welcome-message', ['name'=>'Karunendu'])}}I'm not admin<img src="{{asset('assets/dashboard/img/pages/header.png')}}"/>
 @endhasrole
 
 
